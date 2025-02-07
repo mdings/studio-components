5bbb0e5dd7972a29c0452aef2b0c11f5b98fe63a
Maarten Dings <maartendings@gmail.com>
2025-02-07 15:35:59 +0100

    Update productSlider.jsx

--- components/productSlider.jsx
+++ components/productSlider.jsx
@@ -1,3 +1,10 @@
 function Productslider(){ 
-  return <div>We are testing our component</div>
+  const [name, setName] = useState('Anton');
+  useEffect(() => {
+    setTimeout(()=>{
+      setName('Maarten');
+    }, 3000)
+  }, [])
+  
+  return <div>We are testing our component {name}</div>
 }
