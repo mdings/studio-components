23a6d24d0ec182b0965ace58aa848cc18fce217f
Maarten Dings <maartendings@gmail.com>
2025-02-21 11:48:22 +0100

    Updated remote_newProductSlider.jsx with processed Tailwind styles

--- components/remote_newProductSlider.jsx
+++ components/remote_newProductSlider.jsx
@@ -1,14 +1,14 @@
 function newProductSlider() {
   const [name, setName] = useState("Kees");
-  useEffect(() => {
-    setTimeout(() => {
+  useEffect(() =&gt; {
+    setTimeout(() =&gt; {
       setName("Maarten");
     }, 3000);
   }, []);
 
   return (
-    <div style="{"-TwBgOpacity":"1","backgroundColor":"rgb(255 0 0 / var(--tw-bg-opacity, 1))","fontSize":"1.875rem","lineHeight":"2.25rem","fontWeight":"700","textDecorationLine":"underline"}">
-      <span style="{"-TwBgOpacity":"1","backgroundColor":"rgb(255 0 0 / var(--tw-bg-opacity, 1))","fontSize":"1.875rem","lineHeight":"2.25rem","fontWeight":"700","textDecorationLine":"underline"}">
+    <div style="{}">
+      <span style="{}">
         We are testing our component {name}
       </span>
     </div>
