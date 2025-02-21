3251be9d499f94fe10b0b642ef0d70ec68ca5918
Maarten Dings <maartendings@gmail.com>
2025-02-21 11:25:39 +0100

    Updated remote_newProductSlider.jsx with processed Tailwind styles

--- components/remote_newProductSlider.jsx
+++ components/remote_newProductSlider.jsx
@@ -6,6 +6,5 @@ function newProductSlider() {
     }, 3000);
   }, []);
 
-  return <div style="--tw-bg-opacity: 1;
-  background-color: rgb(255 0 0 / var(--tw-bg-opacity, 1));">We are testing our component {name}</div>;
+  return <div style={"-TwBgOpacity":"1","backgroundColor":"rgb(255 0 0 / var(--tw-bg-opacity, 1))"}>We are testing our component {name}</div>;
 }
